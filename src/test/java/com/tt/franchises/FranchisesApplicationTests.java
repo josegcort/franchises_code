a745fb9b8269d2a3d663b73b2479d83927ed53ff
JoseGCort <josegcort@gmail.com>
2026-04-07 18:51:38 -0500

    Add Maven Wrapper and initial project structure
    - Introduced .gitattributes for line endings
    - Added Maven Wrapper properties and scripts
    - Created initial pom.xml with Spring Boot dependencies
    - Implemented main application class and test class
    - Configured application properties for development profile

---
+++ src/test/java/com/tt/franchises/FranchisesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tt.franchises;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FranchisesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
